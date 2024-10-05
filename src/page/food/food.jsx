81f0c27d73ee4ccea88cced222d3cbc06d1dcd95
Haolu2003 <haoltse182646@fpt.edu.vn>
2024-10-05 20:38:00 +0700

    version 1.1| Update: profile.scss, membership, log out

--- src/page/food/food.jsx
+++ src/page/food/food.jsx
@@ -1,20 +1,32 @@
-
-import Header from '../../components/header/header'
+import { Form, Row } from 'antd';
+import background from '../../img/2.jpg'
 import './food.scss'
-import koi from '../../img/background.jpg';
-function FoodPage() {
+import Header from '../../components/header/header';
+function Food() {
   return (
-    <div className='Foodpage'>
-        <Header/>
-        <div className='Food__img'>
-            <img src={koi} alt="" />
+    <>
+    <Header />
+
+    <div>
+      <div className="Food_Page">
+        <div className='Food_background'>
+          <img src={background} alt="" />
         </div>
-        <div className='Food_body'>
-          <div></div>
-          <div></div>
+
+        <div className='Food_title'>
+          <h3>Food Calculator</h3>
         </div>
+          <div className="Food_form">
+            <Form>
+              <Row>
+                <Col></Col>
+              </Row>
+            </Form>
+          </div>
+      </div>
     </div>
+    </>
   )
 }
 
-export default FoodPage;
+export default Food;
