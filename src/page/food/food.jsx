5a23fe070cb2b1f33a5ef0e52904b50f37dc3422
Haolu2003 <haoltse182646@fpt.edu.vn>
2024-10-12 12:59:10 +0700

    Version 1.1 | Update: food.jsx,food.scss

--- src/page/food/food.jsx
+++ src/page/food/food.jsx
@@ -1,49 +1,126 @@
-import { Col, Form, Input, Row } from 'antd';
-import background from '../../img/2.jpg'
-import './food.scss'
-import Header from '../../components/header/header';
+import background from "../../img/2.jpg";
+import "./food.scss";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import { Button, Form, Input } from "antd";
 function Food() {
   return (
     <>
-    <Header />
+      <Header />
 
-    <div>
-      <div className="Food_Page">
-        <div className='Food_background'>
-          <img src={background} alt="" />
-        </div>
+      <div className="Calculator_background">
+        <img src={background} alt="" />
+      </div>
+      <div className="Calculator_body">
+        <div className="Calculator_title">Food and salt Calculator</div>
+        <div className="Calculator_Form">
+          <div className="Calculator_Food">
+            <Form className="Food_form" layout="vertical">
+              <div className="Food_title">Food calculator</div>
 
-        <div className='Food_title'>
-          <h3>Food Calculator</h3>
-        </div>
-          <div className="Food_body">
-            <Form
-              className="Food_form"
-            >
-              <Form.Item
-                label="Name of your Koi Fish pond: " 
-                name="name"
-                labelCol={{ span: 24 }}
-                wrapperCol={{ span: 24 }} 
-              >
-                <Input/>
+              <Form.Item label="Koi Fish pond name: " name="pondName">
+                <Input placeholder="Enter the name of the koi pond" />
               </Form.Item>
-
-              <Row gutter={16}>
-                <Col xs={24} sm={12}>
-                  <Form.Item
-                  label="Total number of Koi Fish" 
-                  >
-                    <Input/>
+              <div className="form-columns">
+                <div className="form-column">
+                  <Form.Item label="Total number of Koi fish:" name="totalFish">
+                    <Input placeholder="Enter the total number of koi fish" />
                   </Form.Item>
-                </Col>
-              </Row>
+                  <Form.Item
+                    label="Average weight of koi fish (kg):"
+                    name="avgWeight"
+                  >
+                    <Input placeholder="Enter the average weight of Koi fish " />
+                  </Form.Item>
+                </div>
+
+                <div className="form-column">
+                  <Form.Item label="Ratio Food (1% - 4%): " name="feedRatio">
+                    <Input placeholder="Enter feed ratio" />
+                  </Form.Item>
+                  <Form.Item label="Amount of food for the lake (gam):" name="feedAmount">
+                    <Input placeholder="Enter the amount of food for the pond" />
+                  </Form.Item>
+                </div>
+              </div>
+
+              <Form.Item label="Koi Fish name: " name="koiName">
+                <Input placeholder="Enter name of your Koi fish" />
+              </Form.Item>
+              <div className="form-columns">
+                <div className="form-column">
+                  <Form.Item label="Length of your Koi fish (cm):" name="length">
+                    <Input placeholder="Enter length of your Koi fish" />
+                  </Form.Item>
+                  <Form.Item
+                    label="Weight of your Koi fish (kg): "
+                    name="weight"
+                  >
+                    <Input placeholder="Enter weight of your Koi fish " />
+                  </Form.Item>
+                </div>
+
+                <div className="form-column">
+                  <Form.Item label="Tỉ lệ thức ăn" name="feedRatio">
+                    <Input placeholder="Nhập tỉ lệ thức ăn" />
+                  </Form.Item>
+                  <Form.Item label="Lượng thức ăn cho hồ" name="feedAmount">
+                    <Input placeholder="Nhập lượng thức ăn" />
+                  </Form.Item>
+                </div>
+              </div>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Tính toán
+                </Button>
+              </Form.Item>
             </Form>
           </div>
+
+          {/*Salt Calculator */}
+          <div className="Calculator_Salt">
+            <Form className="Food_form" layout="vertical">
+              <div className="Food_title">Food calculator</div>
+
+              <Form.Item label="Tên hồ nuôi cá koi" name="pondName">
+                <Input placeholder="Nhập tên hồ nuôi cá koi" />
+              </Form.Item>
+              <div className="form-columns">
+                <div className="form-column">
+                  <Form.Item label="Tổng số cá koi" name="totalFish">
+                    <Input placeholder="Nhập tổng số cá koi" />
+                  </Form.Item>
+                  <Form.Item
+                    label="Cân nặng trung bình của cá koi"
+                    name="avgWeight"
+                  >
+                    <Input placeholder="Nhập cân nặng trung bình" />
+                  </Form.Item>
+                </div>
+
+                <div className="form-column">
+                  <Form.Item label="Tỉ lệ thức ăn" name="feedRatio">
+                    <Input placeholder="Nhập tỉ lệ thức ăn" />
+                  </Form.Item>
+                  <Form.Item label="Lượng thức ăn cho hồ" name="feedAmount">
+                    <Input placeholder="Nhập lượng thức ăn" />
+                  </Form.Item>
+                </div>
+              </div>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Tính toán
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
       </div>
-    </div>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default Food;
